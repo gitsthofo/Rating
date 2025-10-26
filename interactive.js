51ff048d339957720302b45c6db7d0849cb3e7a9
Mthokozisi Dlamini <107687570+gitsthofo@users.noreply.github.com>
2025-10-26 18:02:26 +0200

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,34 @@
+
+
+
+const mainContainer = document.querySelector(".state");
+const thanksContainer = document.querySelector(".thank-you");
+
+const submitButton = document.getElementById("submit-btn"); 
+const rateAgain = document.getElementById("rate-again");
+
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+
+
+
+submitButton.addEventListener("click", () => {
+
+    thanksContainer.classList.remove("hidden")
+    mainContainer.style.display = "none"
+})
+
+
+rateAgain.addEventListener("click", () => {
+
+    thanksContainer.classList.add("hidden")
+    mainContainer.style.display = "block"
+})
+
+rates.forEach((rate) => {
+
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
